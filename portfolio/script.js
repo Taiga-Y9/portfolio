5ba29c352d870b5cd2b2ae5cbf1c9df7ab58056d
YamazoeTaiga <182177455+Taiga-Y9@users.noreply.github.com>
2024-11-15 09:55:12 +0900

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuIcon = document.getElementById('menu-icon');
-    const navMenu = document.getElementById('nav-menu');
-
-    menuIcon.addEventListener('click', function() {
-        navMenu.classList.toggle('active');  // メニューの表示/非表示を切り替える
-        // アイコンを非表示にする処理を削除
-    });
-});
